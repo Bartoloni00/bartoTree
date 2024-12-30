7d32754c6a542a51da644bb1ba3d236bac946a30
Bartoloni00 <bartoloniabraham@gmail.com>
2024-12-30 08:23:05 -0300

    feat: add --exclude flag and refactor file structure for scalability

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # BartoTree
-Potenciador del comando "ls" de linux o "dir" de windows. con este comando "tree" podemos ver un arbol de todas las carpetas.
+Enhancer for the Linux ls command or the Windows dir command. With the tree command, you can visualize a structured tree view of all directories.
 
 ## Instalación
 
 ```bash
-npm install -g bartotree
+npm link barto-tree
 ```
 
-## Uso
+## Commands
 
 ```bash
-tree
+barto-tree --help
+barto-tree -h
 ```
+
+```bash
+barto-tree tree [rute] [flags]
+```
+
+## Flags
+
+| Flag     | Description                              | Values  | Default |
+| -------- | ---------------------------------------- | ------- | ------- |
+| --depth  | Specifies the maximum depth to display   | number  | infinite |
+| --exclude | Exclude files or folders that match the pattern | string  | ""      |
 
 ## Licencia
 
--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
-import helpOutput from "../services/help.js";
-import displayTree from "../services/tree.js";
-import depthArg from "../services/depthArg.js";
+import helpOutput from "../lib/commands/help.js";
+import displayTree from "../lib/commands/tree.js";
+import depthArg from "../lib/utils/depthArg.js";
 import fs from "fs";
 import path from "path";
+import filterFolders from "../lib/utils/excludeFolders.js";
 const args = process.argv.slice(2);
 const targetDir = args[1] || '.';
 
 let maxDepth = await depthArg(args);
+
+const excludeFolders = filterFolders(args);
 
 if (args.length > 0) {
   const actionArgument = args[0];
@@ -18,11 +21,11 @@ if (args.length > 0) {
       break;
       case "tree":
         if(!fs.existsSync(targetDir)){
-          console.log(`El directorio ${targetDir} no existe`);
+          console.log(`The directory ${targetDir} does not exist.`);
           process.exit(1);
         }
         console.log(`\nTree view for: ${path.resolve(targetDir)}\n`);
-        displayTree(targetDir, 0, maxDepth)
+        displayTree(targetDir, 0, maxDepth, excludeFolders)
         break;
     default:
       helpOutput();
